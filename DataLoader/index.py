cf792ae1be68e56e4f6b1b4bb16c3ece28ae6f86
Ozwin <ozwinneellobo12@gmail.com>
2022-11-01 17:59:10 -0400

    file structure

---
+++ DataLoader/index.py
@@ -0,0 +1,4 @@
+#import this file to train models and pass the  name of the datset
+# extract zip files
+# load the images and labels based on the dataset requested
+# do the pre-processing , batch normalization , flip etc
