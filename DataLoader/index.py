4119cae747c2b70fbfda30b58fd95693380d5486
kirthanasenguttuvan7 <kirthanasenguttuvan7@gmail.com>
2022-11-02 21:52:57 -0400

    DataLoader Code and Dataset_1 added

--- DataLoader/index.py
+++ DataLoader/index.py
@@ -1,4 +1,77 @@
+import os
+
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
 #import this file to train models and pass the  name of the datset
 # extract zip files
 # load the images and labels based on the dataset requested
 # do the pre-processing , batch normalization , flip etc
+
+
+# Transofrmations for preprocessedSnakeImages dataset
+# Reference from https://www.youtube.com/watch?v=z3kB3ISIPAg&list=PL3Dh_99BJkCEhE7Ri8W6aijiEqm3ZoGRq&index=4
+training_path = '/home/keena/PycharmProjects/SnakeSpeciesDetection/DataSets/preprocessed-cleaned-set/preprocessedSnake/train/'
+test_path = '/home/keena/PycharmProjects/SnakeSpeciesDetection/DataSets/preprocessed-cleaned-set/preprocessedSnake/val/'
+val_path = '/home/keena/PycharmProjects/SnakeSpeciesDetection/DataSets/preprocessed-cleaned-set/preprocessedSnake/test/'
+def transformDS1( batchSize, inputSize):
+
+    training_transforms = transforms.Compose([transforms.Resize((inputSize,inputSize)),transforms.ToTensor()])
+    train_dataset = torchvision.datasets.ImageFolder(root=training_path,transform = training_transforms)
+    train_Loader = torch.utils.data.DataLoader(dataset = train_dataset,batch_size=batchSize,shuffle=False)
+
+    mean, std = get_mean_std(train_Loader)
+
+    train_transforms = transforms.Compose([
+        transforms.Resize((inputSize,inputSize)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(10),
+        transforms.ToTensor(),
+        transforms.Normalize(torch.Tensor(mean),torch.Tensor(std))
+    ])
+
+    test_transforms = transforms.Compose([
+        transforms.Resize((inputSize,inputSize)),
+        transforms.ToTensor(),
+        transforms.Normalize(torch.Tensor(mean),torch.Tensor(std))
+    ])
+
+    val_transforms = transforms.Compose([
+        transforms.Resize((inputSize,inputSize)),
+        transforms.ToTensor(),
+        transforms.Normalize(torch.Tensor(mean),torch.Tensor(std))
+    ])
+    train_dataset = torchvision.datasets.ImageFolder(root=training_path,transform=train_transforms)
+    test_dataset = torchvision.datasets.ImageFolder(root=test_path,transform=test_transforms)
+    val_dataset = torchvision.datasets.ImageFolder(root=val_path,transform=val_transforms)
+    return train_dataset,test_dataset,val_dataset
+
+def get_mean_std(loader):
+    mean = 0.
+    std = 0.
+    total_images_count = 0
+    for images,_ in loader:
+        images_count_in_batch = images.size(0)
+        images =images.view(images_count_in_batch,images.size(1),-1)
+        mean+=images.mean(2).sum(0)
+        std+=images.std(2).sum(0)
+        total_images_count+=images_count_in_batch
+    mean /= total_images_count
+    std /= total_images_count
+    return mean,std
+
+def show_transformed_images(dataset):
+    loader = torch.utils.data.DataLoader(dataset,batch_size=6,shuffle=True)
+    batch=next(iter(loader))
+    images,labels = batch
+    grid = torchvision.utils.make_grid(images,nrow=3)
+    plt.figure(figsize=(11,11))
+    plt.imshow(np.transpose(grid,(1,2,0)))
+    plt.show()
+    print("labels:",labels)
+
+
+train_dataset,test_dataset,val_dataset = transformDS1(32,224)
+show_transformed_images(train_dataset)
