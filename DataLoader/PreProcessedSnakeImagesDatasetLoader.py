f0d923cb432d63fb77a26bcb7fd3c61612a1ca74
Ozwin <ozwinneellobo12@gmail.com>
2022-11-07 23:29:18 -0500

    resnet18-update train loop and data loader

--- DataLoader/PreProcessedSnakeImagesDatasetLoader.py
+++ DataLoader/PreProcessedSnakeImagesDatasetLoader.py
@@ -1,5 +1,6 @@
 # Reference for train test and val split https://www.youtube.com/watch?v=C6wbr1jJvVs
 #run pip install split-folders
 import splitfolders
-inputFolder = '/home/keena/PycharmProjects/SnakeSpeciesDetection/DataSets/preprocessed-cleaned-set/train'
-splitfolders.ratio(inputFolder,output="/home/keena/PycharmProjects/SnakeSpeciesDetection/DataSets/preprocessed-cleaned-set/preprocessedSnake",seed=42,ratio=(0.7,0.2,0.1), group_prefix=None)
+from pathlib import Path
+inputFolder = Path('C:\\Personal\\Snake_Species_Recognition\\Dataset\\5-class\\preprocessed-cleaned-set\\train')
+splitfolders.ratio(inputFolder,output="C:\\Personal\\Snake_Species_Recognition\\Dataset\\5-class\\preprocessed-cleaned-set\\data",seed=42,ratio=(0.7,0.2,0.1), group_prefix=None)
